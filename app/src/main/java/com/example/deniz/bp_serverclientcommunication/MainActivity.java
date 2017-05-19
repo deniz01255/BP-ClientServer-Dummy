65ae4d35f3fbb3321dc452dfde55a09cb9c6a509
deniz <deniz-ertan@gmx.de>
2017-05-19 09:11:15 +0200

    fixed git

--- app/src/main/java/com/example/deniz/bp_serverclientcommunication/MainActivity.java
+++ app/src/main/java/com/example/deniz/bp_serverclientcommunication/MainActivity.java
@@ -1,36 +1,55 @@
 package com.example.deniz.bp_serverclientcommunication;
 
+import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.ConsoleMessage;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+import java.util.concurrent.ExecutionException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     // variables for taken the income of the frontend
-    EditText et;
-    Button bt;
-    TextView tv;
+    private EditText et;
+    private Button bt,btPost;
+    public TextView tv;
+    private OkHttpAdress example;
+    private OkHttpSendData examplePost;
+    GeoPoint answer;
+    MapView map;
+    /** Called when the activity is first created. */
+
+
+    String responseString = "";
 
     // final string because of using server-Address always the same.
-    final String scriptUrlString = "http://bp-server.890m.com/receive_script.php";
+    final StringBuilder scriptUrlString = new StringBuilder();//"http://api.openstreetmap.org/api/0.6/trackpoints?bbox=0,51.5,0.25,51.75&page=0";
 
     /**
+     *
      * Method is always created automatically by Developing environment, setting up an interactive Gui connected to backend
      *
      * Function of method on Create(Bundle): - connecting attributes to buttons on our surface
@@ -40,104 +59,118 @@ public class MainActivity extends AppCompatActivity {
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        scriptUrlString.append("http://maps.googleapis.com/maps/api/geocode/json?address=");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         et = (EditText) findViewById(R.id.editText);
+
         bt = (Button) findViewById(R.id.button);
+        btPost = (Button) findViewById(R.id.postButton);
+
         tv = (TextView) findViewById(R.id.textView2);
+        map = (MapView) findViewById(R.id.map);
+        /**
+         * Button to Receive Data From Server
+         */
         bt.setOnClickListener(new View.OnClickListener(){
         @Override
             public void onClick(View v){
-                    if(internetAvailable()){
-                        sendToServer(et.getText().toString());
-                    }else {
-                        Toast.makeText(getApplicationContext(), "Keine Internetverbindung verfügbar!", Toast.LENGTH_SHORT).show();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    scriptUrlString.append(et.getText().toString());
+                    scriptUrlString.append("&sensor=true");
+                    String param = scriptUrlString.toString();
+                    example = new OkHttpAdress(tv,param);
+                    try {
+                        answer = example.execute().get();
+                    }catch(ExecutionException e){
+                        e.printStackTrace();
+                    }catch (InterruptedException i){
+                        i.printStackTrace();
                     }
+                    if(answer == null){
+                        tv.setText("ERROR-MESSAGE: Can't receive Server, please check internet-connection ");
+                    }else {
+
+                        tv.setText("Korrekt");
+
+
+                        Context ctx = getApplicationContext();
+                        //important! set your user agent to prevent getting banned from the osm servers
+                        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+
+                        map.setTileSource(TileSourceFactory.MAPNIK);
+                        map.setBuiltInZoomControls(true);
+                        map.setMultiTouchControls(true);
+                        IMapController mapController = map.getController();
+                        mapController.setZoom(15);
+                        mapController.setCenter(answer);
+
+                    }
+
+                }
+            });
+
              }
         });
-    }
 
-    /**
-     *
-     * Method is Created to Send an given parameter as String to the Server in specific Encoding (UTF-8)
-     *
-     * @param text : The text comes from method onCreate(Bundle) -- Text = text which is typed in on surface
-     */
-    public void sendToServer(final String text){
-        new Thread(new Runnable() {
+
+        /**
+         * Button to Send-Data From Server
+         */
+
+        btPost.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void run() {
-                try {
-                    String textParam = "text1=" + URLEncoder.encode(text,"UTF-8");
+            public void onClick(View v){
 
-                    URL scriptUrl = new URL(scriptUrlString);
-                    HttpURLConnection connection = (HttpURLConnection) scriptUrl.openConnection();
-                    connection.setDoOutput(true);
-                    connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-                    connection.setFixedLengthStreamingMode(textParam.getBytes().length);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                            try {
+                                OkHttpSendData example = new OkHttpSendData();
+                                tv.setText(example.execute().get().toString());
+                            } catch(InterruptedException i){
+                                i.printStackTrace();
+                            } catch(ExecutionException ex){
+                                ex.printStackTrace();
+                            }
 
-                    OutputStreamWriter contentWriter = new OutputStreamWriter(connection.getOutputStream());
-                    contentWriter.write(textParam);
-                    contentWriter.flush();
-                    contentWriter.close();
+                            System.out.println("fertig");
 
-                    InputStream answerInputStream = connection.getInputStream();
-                    final String answer = getTextFromInputStream(answerInputStream);
-
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            tv.setText(answer);
-                        }
-                    });
-
-                    answerInputStream.close();
-                    connection.disconnect();
-
-
-                } catch (MalformedURLException e){
-                    e.printStackTrace();
-                }catch (IOException e){
-                    e.printStackTrace();
-                }
+                    }
+                });
 
             }
-        }).start();
-
-
-
+        });
 
 
     }
 
 
-
-    public String getTextFromInputStream(InputStream is){
-        BufferedReader reader  = new BufferedReader(new InputStreamReader(is));
-        StringBuilder stringBuilder = new StringBuilder();
-
-        String aktuelleZeile;
-        try {
-            while ((aktuelleZeile = reader.readLine()) != null) {
-                    stringBuilder.append(aktuelleZeile);
-                    stringBuilder.append("\n");
-
-
-            }
-        }  catch(IOException e){
-                e.printStackTrace();
-
-            }
-
-            return stringBuilder.toString().trim();
-    }
 
     public boolean internetAvailable(){
         ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
 
+
+
         return networkInfo != null && networkInfo.isConnectedOrConnecting();
     }
+
+
+    public void onResume(){
+        super.onResume();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+    }
+
+
+
 }
